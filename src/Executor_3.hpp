5715bc0a04dbd85514833a90f9d4b710d04afcf2
李茂 <limao1@xiaomi.com>
2021-12-16 12:54:30 +0800

    initial

---
+++ src/Executor_3.hpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <string>
+#include <deque>
+#include <thread>
+#include <condition_variable>
+#include <mutex>
+#include <functional>
+#include <memory>
+#include <chrono>
+
+using Function = std::function<void()>;
+
+// 回调队列，保存进来的数据
+// 背后的线程，不断对队列数据进行处理, 和生产者消费者类似
+// 使用std::bind 构造无参可调用对象，构造回调队列, 主要是为了保存通用的可调用对象，并用容器存储
+// bind 用于生成一个新的可调用对象，比如把参数和函数绑定（保存）在一起，改变参数的个数等
+
+class Executor {
+public:
+
+    Executor() : terminated_(false) {
+        thread_ = std::make_shared<std::thread>(&Executor::spin, this);
+    }
+
+    ~Executor() {
+        std::cout << "~Executor" << std::endl;
+        terminated_ = true;
+        cv_.notify_one();
+        if(thread_) {
+            thread_->join();
+        }
+    }
+
+    void addCallback(Function f){
+       mutex_.lock();
+       queue_.push_back(f);
+       mutex_.unlock();
+
+       cv_.notify_one();
+    }
+
+    void spin() {
+        while(!terminated_) {
+            std::unique_lock<std::mutex> lock(mutex_);
+            cv_.wait(lock, [this](){ if (queue_.empty() || terminated_) return false; else { return true; }});
+
+            auto& t = queue_.front();
+            t();
+            queue_.pop_front();
+            lock.unlock();
+            std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        }
+
+        std::cout << "SPINTHREADEXIT" <<std::endl;
+    }
+    void kill() { terminated_ = true;}
+
+private:
+    bool terminated_;
+    std::deque<Function> queue_;
+    std::mutex mutex_;
+    std::condition_variable cv_;
+    std::shared_ptr<std::thread> thread_;
+};
+
+void one(int i) {
+    std::cout << "one " << i << std::endl;
+}
+
+void two(int i, int j) {
+    std::cout << "two " << i << ", " << j << std::endl;
+}
+
+// int main() {
+//     Executor e;
+
+//     for(int i = 0; i < 10; ++i) {
+//       std::cout << "add " << i << std::endl;
+//       e.addCallback(std::bind(one, i));
+//     }
+
+//     // std::this_thread::sleep_for(std::chrono::seconds(22));
+
+//     for(int i = 5; i < 10; ++i) {
+//       std::cout << "add " << i << std::endl;
+//       e.addCallback(std::bind(two, i, i * 10));
+//     }
+//     std::this_thread::sleep_for(std::chrono::seconds(18));
+
+//     // e.kill();
+
+//     for(int i = 100; i < 105; ++i) {
+//       std::cout << "add " << i << std::endl;
+//       e.addCallback(std::bind(one, i));
+//     }
+
+//     std::this_thread::sleep_for(std::chrono::seconds(6));
+
+//     std::cout << "finish" << std::endl;
+
+
+// }
---
+++ src/Locomotor.cpp
@@ -0,0 +1,177 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <memory>
+#include <vector>
+#include <chrono>
+
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp/node.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+
+#include "./Executor_3.hpp"
+
+using GlobalPlanCallback = std::function<void ()>;
+using GlobalPlanExceptionCallback = std::function<void ()>;
+using LocalPlanCallback = std::function<void ()>;
+using LocalPlanExceptionCallback = std::function<void ()>;
+using PlannerExceptionCallback = std::function<void ()>;
+
+
+class Locomotor : public rclcpp::Node
+{
+public:
+  Locomotor()
+  : Node("locomotor")
+  {
+    g_cnt = 0;
+    l_cnt = 0;
+
+    goal_sub =
+      create_subscription<geometry_msgs::msg::PoseStamped>(
+      "/goal_pose", 10,
+      std::bind(&Locomotor::topic_callback, this, std::placeholders::_1));
+  }
+
+  ~Locomotor()
+  {
+    if (g_timer) {
+      g_timer->cancel();
+      g_timer.reset();
+    }
+
+    if (l_timer) {
+      l_timer->cancel();
+      l_timer.reset();
+    }
+  }
+
+  void topic_callback(const geometry_msgs::msg::PoseStamped::SharedPtr p)
+  {
+    RCLCPP_INFO(get_logger(), "goal receive! start");
+    g_cnt = 0;
+    l_cnt = 0;
+    if (!g_timer) {
+      g_timer =
+        create_wall_timer(
+        std::chrono::milliseconds(1000), std::bind(
+          &Locomotor::planLoopCallback,
+          this));
+    }
+  }
+
+  void planLoopCallback()
+  {
+    requestGlobalPlan(
+      g_ex, l_ex, std::bind(&Locomotor::onNewGlobalPlan, this),
+      std::bind(&Locomotor::onGlobalPlanningException, this));
+  }
+  void controlLoopCallback()
+  {
+    requestLocalPlan(
+      g_ex, l_ex, std::bind(&Locomotor::onNewLocalPlan, this),
+      std::bind(&Locomotor::onLocalPlanningException, this));
+
+  }
+
+  void requestGlobalPlan(
+    Executor & work_ex, Executor & result_ex, GlobalPlanCallback cb = nullptr,
+    GlobalPlanExceptionCallback fail_cb = nullptr)
+  {
+    work_ex.addCallback(
+      std::bind(
+        &Locomotor::makeGlobalPlan, this, std::ref(
+          result_ex), cb, fail_cb));
+  }
+
+  void makeGlobalPlan(Executor & result_ex, GlobalPlanCallback cb, PlannerExceptionCallback fail_cb)
+  {
+    std::cout << "makeGlobalPlan =>>" << std::endl;
+    if (cb) {
+      result_ex.addCallback(cb);
+    }
+  }
+
+
+  void onNewGlobalPlan()
+  {
+    std::cout << "GP YES " << g_cnt++ << std::endl;
+    if (g_cnt > 10) {
+      std::cout << "GP FINISH " << g_cnt << std::endl;
+      g_timer->cancel();
+      // g_timer->reset();
+      // g_timer.reset();  // exit when reset ??
+      if (!g_timer->is_canceled()) {
+        std::cout << "g_timer canceled, but ??" << std::endl;
+      }
+
+      if (g_timer) {
+        std::cout << "GP FINISH BUT g-timer pointer is true" << std::endl;
+      }
+    }
+
+
+    if (!l_timer) {
+      std::cout << "Create Local Timer" << std::endl;
+      l_timer = create_wall_timer(
+        std::chrono::milliseconds(250), std::bind(
+          &Locomotor::controlLoopCallback,
+          this));
+    }
+  }
+
+  void onGlobalPlanningException()
+  {
+    std::cerr << "GP FAILED xx" << std::endl;
+  }
+
+  void requestLocalPlan(
+    Executor & work_ex, Executor & result_ex, LocalPlanCallback cb = nullptr,
+    LocalPlanExceptionCallback fail_cb = nullptr)
+  {
+    work_ex.addCallback(
+      std::bind(
+        &Locomotor::makeLocalPlan, this, std::ref(result_ex), cb, fail_cb));
+  }
+
+  void makeLocalPlan(Executor & result_ex, LocalPlanCallback cb, PlannerExceptionCallback fail_cb)
+  {
+    std::cout << "makeLocalPlan =>>" << std::endl;
+    if (cb) {
+      result_ex.addCallback(cb);
+    }
+  }
+
+  void onNewLocalPlan()
+  {
+    std::cout << "LP YES " << l_cnt++ << std::endl;
+    if (l_cnt > 50) {
+      std::cout << "LP FINISH " << l_cnt << std::endl;
+      l_timer->cancel();
+      l_timer.reset();   // whey EXIT ??
+    }
+  }
+
+  void onLocalPlanningException()
+  {
+    std::cerr << "LP FAILED xx" << std::endl;
+  }
+
+private:
+  rclcpp::TimerBase::SharedPtr g_timer, l_timer;
+  rclcpp::SubscriptionBase::SharedPtr goal_sub;
+
+  int g_cnt;
+  int l_cnt;
+
+  Executor g_ex;
+  Executor l_ex;
+};
+
+
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<Locomotor>());
+  rclcpp::shutdown();
+}
